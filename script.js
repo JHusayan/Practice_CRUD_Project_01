7a118eccd4debe196c56d63d78ae471925499b1a
JHusayan <joshua.alex.husayan@gmail.com>
2022-06-01 13:31:03 +0800

    Added JS functionality

---
+++ script.js
@@ -0,0 +1,29 @@
+var app = new function(){
+    this.element= document.getElementById('registry');
+    this.registry=[]
+    /*Refresh Function*/
+    this.FetchAll = function(){
+        var data='';
+        
+    };
+    /*Create Function*/
+    this.Add = function(){
+        
+    };
+    /*Update Function*/
+    this.Edit = function(item){
+
+    };
+    /*Delete Function*/
+    this.Delete = function(item){
+
+    };
+    /*Display Function*/
+    this.Count = function(data){
+        
+    };
+}
+app.FetchAll();
+function CloseInput(){
+    document.getElementById('edit-box').style.display ='none';
+}
